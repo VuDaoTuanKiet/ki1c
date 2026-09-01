da4a7bdd684a9c10f759c536b833062fff9251af
VuDaoTuanKiet <vudaotuankiet0601@gmail.com>
2026-09-01 11:27:20 +0700

    modified:   C03061.c

--- C03061.c
+++ C03061.c
@@ -1,14 +1,33 @@
 //spiderman will save me and bug in my code
 #include<stdio.h>
+#include<string.h>
 
 typedef long long ll;
 
+int check(char s[])
+{
+    int n = strlen(s);
+   if (2 * (s[0] - '0') != (s[n - 1] - '0') && (s[0] - '0')!= 2*(s[n - 1] - '0')) return 0;
+    for(int i =1;i<n/2;i++)
+    {
+        if(s[i] != s[n - 1 - i])
+        return 0;
+    }
+    return 1;
+}
+
+
 signed main()
 {
-    int a,b;
-    scanf("%lld %lld", &a, &b);
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        char s[25];
+        scanf("%s", s);
 
-    printf("%lld %lld ", a ,b);
+        printf((check(s))?"YES\n":"NO\n");
+    }
 
 return 0;
 }
